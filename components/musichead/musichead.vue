6e00c396c843f30c6e419b863dffac3642c8bd8b
Lei10 <1019369786@qq.com>
2021-08-09 10:14:51 +0800

    uniapp-MusicTest

---
+++ components/musichead/musichead.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="music-head">
+		<view v-if="icon" class="music-head-icon">
+			<text class="iconfont iconzuojiantou-copy" @tap="handleToBack"></text> | <text class="iconfont iconshouye" @tap="handleToHome"></text>
+		</view>
+		{{ title }}
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"musichead",
+		data() {
+			return {
+				
+			};
+		},
+		props : ['title','icon'],
+		methods : {
+			handleToBack(){
+				uni.navigateBack();
+			},
+			handleToHome(){
+				uni.navigateTo({
+					url: '/pages/index/index',
+				});
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.music-head{
+		width: 100%;
+		height: 75px;
+		font-size: 16px;
+		line-height: 80px;
+		text-align: center;
+		color: #000000;
+		position: relative;
+	}
+	.music-head-icon{
+		position: absolute;
+		left: 8px;
+		top: 26px;
+		width: 80px;
+		height: 30px;
+		background: rgba(0,0,0,0.4);
+		color: #FFFFFF;
+		line-height: 31px;
+		border-radius: 15px;
+		display: flex;
+		justify-content: space-evenly;
+	}
+</style>
