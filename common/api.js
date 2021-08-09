454bddc99bbb948572f080942be8fe764d62c17b
Lei10 <1019369786@qq.com>
2021-08-09 10:14:51 +0800

    uniapp-MusicTest

---
+++ common/api.js
@@ -0,0 +1,22 @@
+import { baseUrl } from './config.js'
+
+export function topList(){
+	
+	
+	return new Promise(function(resolve,reject){
+		
+		uni.request({
+			url: `${baseUrl}/toplist/detail`,
+			method: 'GET',
+			data: {},
+			success: res => {
+				let result = res.data.list;
+				result.length = 4;
+				resolve(result);
+			},
+			fail: () => {},
+			complete: () => {}
+		});
+	});
+
+}
